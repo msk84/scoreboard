f2aab4db4109f331ab3b9b9d2016706c98d9a1f4
msk84 <florian@steinle.it>
2020-10-27 23:10:13 +0100

    Get things (SpringBoot, jquery, bootstrap) up to date and working.

--- src/main/java/net/msk/scoreboard/config/SecurityConfig.java
+++ src/main/java/net/msk/scoreboard/config/SecurityConfig.java
@@ -11,9 +11,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.anyRequest()
-				.permitAll()
-				.and().csrf().disable();
+		http
+				.authorizeRequests()
+				.antMatchers("/css/**", "/js/**")
+				.permitAll();
 	}
 }
